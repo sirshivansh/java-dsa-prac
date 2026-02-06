c3130c4d6a4c124d18662c52d928a734eb8334ca
Shivansh Mishra <shivnsh01@gmail.com>
2026-02-07 00:59:03 +0530

    Day 01: Main, Java Variables, Output

---
+++ day01/MainMethod.java
@@ -0,0 +1,6 @@
+public class MainMethod {
+    public static void main(String[] args){
+        System.out.println("Day 1 of 100 Days of Code");
+        System.out.println("Main method samajh aa gaya");
+    }
+}
